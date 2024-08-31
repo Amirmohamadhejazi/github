393a2b04a102296a698a63058c8c99a0b28210da
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-08-31 15:13:45 +0330

    chore: layout and landing template

--- src/components/partials/layouts/RootLayout/RootLayout.tsx
+++ src/components/partials/layouts/RootLayout/RootLayout.tsx
@@ -2,12 +2,12 @@ import { type PropsWithChildren } from 'react'
 
 const RootLayout = ({ children }: PropsWithChildren) => {
     return (
-        <>
-            <div className='w-full border-b border-white text-center'>
+        <div className='flex flex-col h-screen  '>
+            <div className='w-full border-b border-[#475161] text-center'>
                 <span className='text-2xl'>header</span>
             </div>
             {children}
-        </>
+        </div>
     )
 }
 
--- src/templates/LandingTemplate/LandingTemplate.tsx
+++ src/templates/LandingTemplate/LandingTemplate.tsx
@@ -1,10 +1,14 @@
 const LandingTemplate = () => {
     return (
-        <div className='flex h-full'>
-            <div className='w-full max-w-[335px] border-r border-white bg-[#30363db3]'>s</div>
-            <div className='grow h-full'>
-                <div className='w-full max-w-[1299px] h-full bg-purple-600 mx-auto overflow-auto'>
-                    <div className='w-2 h-[10000px] bg-red-500'>s</div>
+        <div className='flex grow overflow-auto *:py-4 *:px-6'>
+            {/* <div className='w-2 h-[10000px] bg-red-500'>s</div> */}
+            <div className='w-full flex max-w-[335px] border-r border-[#475161] bg-[#30363db3]  overflow-auto'>
+                <span className='size-full bg-blue-900'>A</span>
+            </div>
+            <div className='flex grow  overflow-auto'>
+                <div className='w-full grid grid-cols-7 gap-3'>
+                    <div className='col-span-5 bg-slate-500'>content</div>
+                    <div className='col-span-2 bg-slate-600'>content</div>
                 </div>
             </div>
         </div>
